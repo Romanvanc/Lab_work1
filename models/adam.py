2bc4c14f49b7f73b64ed53dc7cf0ad68be9085f9
Romanvanc <joshweah160@gmail.com>
2026-04-29 10:56:31 +0300

    Create adam.py

---
+++ models/adam.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import models
+
+def get_model(num_classes=120):
+    model = models.convnext_tiny(weights=models.ConvNeXt_Tiny_Weights.DEFAULT)
+    in_features = model.classifier[2].in_features
+    model.classifier[2] = nn.Linear(in_features, num_classes)
+    
+    for name, param in model.named_parameters():
+        if 'classifier.2' not in name:
+            param.requires_grad = False
+    
+    return model
+
+def get_optimizer(model):
+    return optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-4)
