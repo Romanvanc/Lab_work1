03629b5ed92bfb863e2f486d275f4a8ec8168637
Romanvanc <joshweah160@gmail.com>
2026-04-29 10:53:25 +0300

    Create dataset.py

---
+++ utils/dataset.py
@@ -0,0 +1,48 @@
+import torch
+from torch.utils.data import DataLoader, random_split
+from torchvision import datasets, transforms
+
+def get_transforms():
+    train_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    
+    val_test_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    
+    return train_transform, val_test_transform
+
+def load_datasets(data_path='stanford_dogs/images/Images'):
+    train_transform, val_test_transform = get_transforms()
+    
+    dataset = datasets.ImageFolder(root=data_path, transform=train_transform)
+    num_classes = len(dataset.classes)
+    class_names = dataset.classes
+    
+    total = len(dataset)
+    train_size = int(0.7 * total)
+    val_size = int(0.15 * total)
+    test_size = total - train_size - val_size
+    
+    train_dataset, val_dataset, test_dataset = random_split(
+        dataset, [train_size, val_size, test_size],
+        generator=torch.Generator().manual_seed(497328)
+    )
+    
+    val_dataset.dataset.transform = val_test_transform
+    test_dataset.dataset.transform = val_test_transform
+    
+    return train_dataset, val_dataset, test_dataset, num_classes, class_names
+
+def create_loaders(train_dataset, val_dataset, test_dataset, batch_size=32):
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2, pin_memory=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=2, pin_memory=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2, pin_memory=True)
+    
+    return train_loader, val_loader, test_loader
