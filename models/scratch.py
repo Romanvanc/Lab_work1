f0fe5cdc0b123b3d88575b100ee5c72f22bba05c
Romanvanc <joshweah160@gmail.com>
2026-05-16 14:24:42 +0300

    Update scratch.py

--- models/scratch.py
+++ models/scratch.py
@@ -1,11 +1,55 @@
 import torch.nn as nn
-import torch.optim as optim
-from torchvision import models
 
-def get_model(num_classes=120):
-    model = models.convnext_tiny(weights=None)
-    in_features = model.classifier[2].in_features
-    model.classifier[2] = nn.Linear(in_features, num_classes)
+
+class ScratchCNN(nn.Module):
+    def __init__(self, num_classes=120):
+        super().__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(128, 256, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(256, 512, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d((1, 1))
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Dropout(p=0.3),
+            nn.Linear(512, 256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.3),
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
+
+
+def get_scratch_cnn(num_classes=120):
+    model = ScratchCNN(num_classes=num_classes)
+    print(f"Custom ScratchCNN model created from scratch: 3 -> {num_classes}")
     return model
 
 def get_optimizer(model):
